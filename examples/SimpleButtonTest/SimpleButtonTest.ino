0308c61e7bb1b10bb28b58bc85c31d980bab07f2
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-12-11 13:52:03 +0900

    Update SimpleButtonTest.ino

--- examples/SimpleButtonTest/SimpleButtonTest.ino
+++ examples/SimpleButtonTest/SimpleButtonTest.ino
@@ -1,9 +1,11 @@
 #include <RiaPushbuttonLite.h>
 
-// 예: Zumo USER 버튼이 12번 핀, 풀업 스위치(눌리면 GND)
+// 예: 버튼(스위치)이 12번 핀에 연결되었다고 가정, 버튼(스위치)가 풀업 회로로 구성되었다고 가정했을 때
+// 눌리면 GND, LOW / 누르지 않으면 Vcc, HIGH
 //     - mode = INPUT_PULLUP
 //     - pressedLevel = LOW
-//     - debounceMs = 30ms
+//     - debounceMs = 30ms     
+// debouncdMs의 값은 버튼(스위치)의 특성에 따라 30ms 보다 작게 설정할 수 있음.
 RiaPushbuttonLite button(12, INPUT_PULLUP, LOW, 30);
 
 void setup() {
